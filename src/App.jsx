4269765b5a52d53a01b2c875635291e110731ed0
fatemzh <abid.fatem@hotmail.fr>
2024-02-14 17:12:18 +0100

    Skills part added and images bugs fixed
    added "require" in the data file + corrected "icon" to "iconUrl"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
+import Skills from './components/Skills/Skills';
 
 const App = () => {
   return(
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar/>
       <div className='container'>
         <Hero/>
+        <Skills/>
       </div>
     </>
   )
---
+++ src/utils/data.js
@@ -0,0 +1,40 @@
+export const SKILLS = [
+    {
+        title: "Frontend",
+        iconUrl: require("../assets/images/frontend.png"),
+        skills:[
+            {skill: "HTML5", percentage: "80%"},
+            {skill: "CSS3 (Sass/SCSS)", percentage: "100%"},
+            {skill: "JavaScript (ES6)", percentage: "70%"},
+            {skill: "ReactJS", percentage: "60%"},
+        ],
+    },
+    {
+        title: "Backend",
+        iconUrl: require("../assets/images/backend.png"),
+        skills:[
+            {skill: "NodeJS", percentage: "60%"},
+            {skill: "Php", percentage: "80%"},
+            {skill: "MySQL", percentage: "60%"},
+        ],
+    },
+    {
+        title: "Tools",
+        iconUrl: require("../assets/images/tools.png"),
+        skills:[
+            {skill: "Git & Github", percentage: "85%"},
+            {skill: "VS Code", percentage: "90%"},
+            {skill: "NPM", percentage: "70%"},
+            {skill: "Responsive Design", percentage: "80%"},
+        ],
+    },
+    {
+        title: "Soft Skills",
+        iconUrl: require("../assets/images/softSkill.png"),
+        skills:[
+            {skill: "Problem-solving", percentage: "90%"},
+            {skill: "Time Management", percentage: "85%"},
+            {skill: "Attention to Detail", percentage: "90%"},
+        ]
+    }
+];
